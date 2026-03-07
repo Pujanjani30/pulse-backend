2de56e92b1c15b0ac0b80b3d862262095a8bd819
Pujan Jani <123894599+Pujanjani30@users.noreply.github.com>
2026-03-07 19:17:48 +0530

    ci: update GitHub Actions workflow to use pnpm

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8000);
+
+  Logger.log(
+    `Application is running on: http://localhost:${process.env.PORT ?? 8000}`,
+  );
 }
 bootstrap();
