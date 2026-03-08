dba0e4c6ab5cd10385d03edc8c79d30f454f54db
Pujan Jani <123894599+Pujanjani30@users.noreply.github.com>
2026-03-08 17:24:27 +0530

    chore(config): setup environment configuration and validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from '@modules/users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { envValidationSchema } from '@config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      validationSchema: envValidationSchema,
     }),
     UsersModule,
   ],
